3b8a5c27cde2f16cdc134d90851acc47450aa2ba
gab.chibuye@gmail.com <gab.chibuye@gmail.com>
2023-07-04 18:17:20 +0200

    Removed the main class and added comments

--- exercise1.py
+++ exercise1.py
@@ -1,13 +1,11 @@
-
-#def main():
-#Happiness Variables
+#First exercise, creating a variable, collecting input fro user and printing the output 
+#This is a new copy from the local file to gitHub!!
 ice_cream_rating = int(input("Enter ice cream rating between 0 and 10: "));
 sleeping_rating = int(input("Enter your sleeping rating between 0 and 10: "))
 
 happiness_rating = (ice_cream_rating + sleeping_rating)/2
 rating_percent = happiness_rating*10
-    
-
+   
 #Name variable
 first_name = str(input("Your first name: "))
 last_name = str(input("Your last name: " ))
@@ -18,5 +16,3 @@ print("My name is ", my_name, "my ice cream rating is ", ice_cream_rating)
 print("My sleeping rating is ", sleeping_rating, " out of 10")
 print("Based on the factors above, my happiness rating is :", happiness_rating, " out of 10, or ", rating_percent, "%")
 
-#if __name__ == "__main__":
- #   main()
